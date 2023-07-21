78b5c634986140487f6e4c8c05afb0b77527816b
Emre Özdin <mrzdnboun@gmail.com>
2023-07-21 10:24:08 +0300

    mongo_started

--- REACT/026 - inventory-app/backend/server.js
+++ REACT/026 - inventory-app/backend/server.js
@@ -6,15 +6,16 @@ const cors = require("cors");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 //connect to DB and start server
-
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+      console.log(`Server started on port ${PORT}`);
     });
   })
-  .catch((err) => console.log("err"));
+  .catch((err) => {
+    console.log(err);
+  });
