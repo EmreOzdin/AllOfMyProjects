eb26c61da980173eb957fed90e37ebf72b3bf2e2
Emre Özdin <mrzdnboun@gmail.com>
2023-07-22 11:23:41 +0300

    custom error handler

---
+++ REACT/026 - inventory-app/backend/controllers/userController.js
@@ -0,0 +1,11 @@
+const registerUser = async (req, res) => {
+  if (!req.body.email) {
+    res.status(400);
+    throw new Error("Please add an email");
+  }
+  res.send("Register User");
+};
+
+module.exports = {
+  registerUser,
+};
