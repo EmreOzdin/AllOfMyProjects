967f5938031a6da3a5d40c330aa6ab801ba95380
Emre Özdin <mrzdnboun@gmail.com>
2023-08-08 16:52:25 +0300

    deletetodos added

--- REACT/027 - To-Do-List-Vite/src/App.jsx
+++ REACT/027 - To-Do-List-Vite/src/App.jsx
@@ -7,15 +7,30 @@ export default function App() {
 
   function handleSubmit(e) {
     e.preventDefault();
-
     setTodos((currentTodos) => {
       return [
         ...currentTodos,
         { id: crypto.randomUUID(), title: newItem, completed: false },
       ];
     });
+    setNewItem("");
   }
-  console.log(todos);
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
 
   return (
     <>
@@ -34,21 +49,25 @@ export default function App() {
       <h1 className="header">Todo List</h1>
       <ul className="list">
         {todos.map((todo) => {
-          <li>
-            <label>
-              <input type="checkbox" />
-              {todo}
-            </label>
-            <button className="btn btn-danger">Delete</button>
-          </li>;
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+              </label>
+              <button
+                onClick={() => deleteTodo(todo.id)}
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
+            </li>
+          );
         })}
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 2
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
       </ul>
     </>
   );
