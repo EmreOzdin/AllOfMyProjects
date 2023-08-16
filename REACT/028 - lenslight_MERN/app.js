b1b88591aa329fe22e4898c52332e7b7f0037bde
Emre Özdin <mrzdnboun@gmail.com>
2023-08-16 11:04:07 +0300

    route added

--- REACT/028 - lenslight_MERN/app.js
+++ REACT/028 - lenslight_MERN/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import conn from "./db.js";
+import pageRoute from "./routes/pageRoute.js";
 
 dotenv.config();
 
@@ -8,23 +9,26 @@ dotenv.config();
 conn();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 //ejs template engine
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 //static files middleware
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+//routes
+app.use("/", pageRoute);
 
-app.get("/about", (req, res) => {
-  res.render("about");
-});
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+
+// app.get("/about", (req, res) => {
+//   res.render("about");
+// });
 
 app.listen(port, () => {
   console.log(`Application running on port: ${port}`);
-});
+}); 
