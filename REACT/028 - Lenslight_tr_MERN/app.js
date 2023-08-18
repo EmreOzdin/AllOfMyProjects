2f5856ba70b318d27173abf959cd2e5e90c78649
Emre Özdin <mrzdnboun@gmail.com>
2023-08-18 18:00:16 +0300

    model created

--- REACT/028 - Lenslight_tr_MERN/app.js
+++ REACT/028 - Lenslight_tr_MERN/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import conn from "./db.js";
+import pageRoute from "./routes/pageRoute.js";
 
 dotenv.config();
 
@@ -16,13 +17,16 @@ app.set("view engine", "ejs");
 //static files middleware
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+//routes
+app.use("/", pageRoute)
 
-app.get("/about", (req, res) => {
-  res.render("about");
-});
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+
+// app.get("/about", (req, res) => {
+//   res.render("about");
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
