93191f36201730b7e63eb2340af2e464aeae4f6f
Emre Özdin <mrzdnboun@gmail.com>
2023-08-21 11:50:49 +0300

    get and post req added

--- REACT/028 - Lenslight_tr_MERN/controllers/photoController.js
+++ REACT/028 - Lenslight_tr_MERN/controllers/photoController.js
@@ -1,11 +1,33 @@
 import Photo from "../models/photoModel.js";
 
-const createPhoto = (req, res) => {
-  const photo = Photo.create(req.body);
-  res.status(201).json({
-    succeeded: true,
-    photo,
-  });
+const createPhoto = async (req, res) => {
+  try {
+    const photo = await Photo.create(req.body);
+    res.status(201).json({
+      succeeded: true,
+      photo,
+    });
+  } catch (error) {
+    res.status(500).json({
+      succeeded: false,
+      error,
+    });
+  }
 };
 
-export { createPhoto };
+const getAllPhotos = async (req, res) => {
+  try {
+    const photos = await Photo.find({});
+    res.status(200).json({
+      succeeded: true,
+      photos,
+    });
+  } catch (error) {
+    res.status(500).json({
+      succeeded: false,
+      error,
+    });
+  }
+};
+
+export { createPhoto, getAllPhotos };
