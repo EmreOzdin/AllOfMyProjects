0c3c621d210acac66aed998f470bec58a13c2aa9
Emre Özdin <mrzdnboun@gmail.com>
2023-08-24 16:29:21 +0300

    auth added

--- REACT/028 - Lenslight_tr_MERN/controllers/userController.js
+++ REACT/028 - Lenslight_tr_MERN/controllers/userController.js
@@ -32,10 +32,13 @@ const loginUser = async (req, res) => {
       });
     }
     if (same) {
-      res.status(200).json({
-        user,
-        token: createToken(user._id),
+      const token = createToken(user._id);
+      res.cookie("jwt", token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24,
       });
+
+      res.redirect("/users/dashboard");
     } else {
       res.status(500).json({
         succeeded: false,
@@ -56,4 +59,10 @@ const createToken = (userId) => {
   });
 };
 
-export { createUser, loginUser };
+const getDashboardPage = (req, res) => {
+  res.render("dashboard", {
+    link: "dashboard",
+  });
+};
+
+export { createUser, loginUser, getDashboardPage };
--- REACT/028 - Lenslight_tr_MERN/middlewares/authMiddleware.js
+++ REACT/028 - Lenslight_tr_MERN/middlewares/authMiddleware.js
@@ -3,22 +3,19 @@ import jwt from "jsonwebtoken";
 
 const authenticateToken = async (req, res, next) => {
   try {
-    const token =
-      req.headers["authorization"] &&
-      req.headers["authorization"].split(" ")[1];
-
-    if (!token) {
-      return res.status(401).json({
-        success: false,
-        error: "No token available",
+    const token = req.cookies.jwt;
+    if (token) {
+      jwt.verify(token, process.env.JWT_SECRET, (err) => {
+        if (err) {
+          console.log(err.message);
+          res.redirect("/login");
+        } else {
+          next();
+        }
       });
+    } else {
+      res.redirect("/login");
     }
-
-    req.user = await User.findById(
-      jwt.verify(token, process.env.JWT_SECRET).userId
-    );
-
-    next();
   } catch (error) {
     res.status(401).json({
       success: false,
