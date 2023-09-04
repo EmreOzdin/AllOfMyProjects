a45633f9f679e4243e986fff63f5cad3c4a3d35f
Emre Özdin <mrzdnboun@gmail.com>
2023-09-04 11:04:27 +0300

    numOfUser and numOfPhotos added

--- REACT/028 - Lenslight_tr_MERN/controllers/pageController.js
+++ REACT/028 - Lenslight_tr_MERN/controllers/pageController.js
@@ -1,8 +1,17 @@
 import nodemailer from "nodemailer";
+import Photo from "../models/photoModel.js";
+import User from "../models/userModel.js";
 
-const getIndexPage = (req, res) => {
+const getIndexPage = async (req, res) => {
+  const photos = await Photo.find().sort({ uploadedAt: -1 }).limit(3);
+
+  const numOfUser = await User.countDocuments({});
+  const numOfPhotos = await User.countDocuments({});
   res.render("index", {
     link: "index",
+    photos,
+    numOfUser,
+    numOfPhotos,
   });
 };
 
